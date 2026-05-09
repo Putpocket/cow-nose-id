89d47c43dae02bfef0100e9981083c769e134141
deay0311 <deay0311@gmail.com>
2026-05-10 01:02:27 +0900

    Delete services directory
    통합 파일 업로드를 위한 기존 파일 제거

--- services/dino.py
+++
@@ -1,8 +0,0 @@
-# DINO 모델을 이용해 벡터를 추출하는 함수
-def extract_vector(image):
-    # image: crop된 이미지 객체
-
-    # TODO: 실제 DINO 모델 연결 필요
-
-    # 테스트용 더미 벡터 반환
-    return [0.1, 0.2, 0.3]
--- services/yolo.py
+++
@@ -1,9 +0,0 @@
-# YOLO 모델을 통해 비문 위치를 찾는 함수
-def detect_nose(image_path):
-    # image_path: 입력 이미지 경로
-
-    # TODO: 실제 YOLO 모델 연결 필요
-    # 현재는 테스트용으로 고정된 bbox 반환
-
-    # [x1, y1, x2, y2] 형태의 좌표
-    return [100, 100, 200, 200]
